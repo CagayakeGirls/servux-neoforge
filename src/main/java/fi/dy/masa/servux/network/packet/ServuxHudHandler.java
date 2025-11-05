987a04222c09beea66cb1c801cf78806177f56f5
TexTrue <65154269+TexBlock@users.noreply.github.com>
2025-11-05 22:25:42 +0800

    re-port 1.21.1 neoforge

--- src/main/java/fi/dy/masa/servux/network/packet/ServuxHudHandler.java
+++ src/main/java/fi/dy/masa/servux/network/packet/ServuxHudHandler.java
@@ -10,8 +10,8 @@ import net.minecraft.network.packet.CustomPayload;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.api.distmarker.OnlyIn;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 
 import fi.dy.masa.servux.Reference;
@@ -21,7 +21,7 @@ import fi.dy.masa.servux.network.IPluginServerPlayHandler;
 import fi.dy.masa.servux.network.IServerPayloadData;
 import fi.dy.masa.servux.network.PacketSplitter;
 
-@Environment(EnvType.SERVER)
+@OnlyIn(Dist.DEDICATED_SERVER)
 public abstract class ServuxHudHandler<T extends CustomPayload> implements IPluginServerPlayHandler<T>
 {
     private static final ServuxHudHandler<ServuxHudPacket.Payload> INSTANCE = new ServuxHudHandler<>() {
