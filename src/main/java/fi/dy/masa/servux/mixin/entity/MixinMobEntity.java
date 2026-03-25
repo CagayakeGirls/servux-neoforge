44647712d473d6d3dc2fb9761251fa58f3d4ba42
TexTrue <65154269+TexBlock@users.noreply.github.com>
2026-03-25 15:32:38 +0800

    port to 1.21.11 neoforge

--- src/main/java/fi/dy/masa/servux/mixin/entity/MixinMobEntity.java
+++ src/main/java/fi/dy/masa/servux/mixin/entity/MixinMobEntity.java
@@ -3,11 +3,10 @@ package fi.dy.masa.servux.mixin.entity;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
-import net.minecraft.world.level.gamerules.GameRule;
-import net.minecraft.world.level.gamerules.GameRules;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -38,16 +37,16 @@ public abstract class MixinMobEntity
 	@SuppressWarnings("unchecked")
 	@WrapOperation(method = "aiStep",
 	               at = @At(value = "INVOKE",
-	                   target = "Lnet/minecraft/world/level/gamerules/GameRules;get(Lnet/minecraft/world/level/gamerules/GameRule;)Ljava/lang/Object;"))
-	private <T> T servux$fixAllayGathering4(GameRules instance, GameRule<T> gameRule, Operation<T> original)
+	                   target = "Lnet/neoforged/neoforge/event/EventHooks;canEntityGrief(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/entity/Entity;)Z"))
+	private boolean servux$fixAllayGathering4(ServerLevel level, Entity entity, Operation<Boolean> original)
 	{
 		if (EntitiesDataProvider.INSTANCE.hasFixAllayGathering() &&
 			this.isAllay)
 		{
-			return (T) (Object) true;
+			return true;
 		}
 
 		this.isAllay = false;
-		return instance.get(gameRule);
+		return original.call(level, entity);
 	}
 }
