5726920a1ee5e4abc586cb4d0440443c253904e4
Asineth <asineth@pm.me>
2026-07-14 18:48:18 -0400

    Fix NeoForge 1.21.1 mixin patch

--- src/main/java/fi/dy/masa/servux/mixin/entity/MixinAllayEntity.java
+++ src/main/java/fi/dy/masa/servux/mixin/entity/MixinAllayEntity.java
@@ -1,10 +1,10 @@
 package fi.dy.masa.servux.mixin.entity;
 
-import net.minecraft.entity.ai.brain.Brain;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.passive.AllayEntity;
-import net.minecraft.world.GameRules;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
@@ -13,29 +13,17 @@ import fi.dy.masa.servux.dataproviders.EntitiesDataProvider;
 @Mixin(AllayEntity.class)
 public abstract class MixinAllayEntity
 {
-//	@Shadow public abstract Brain<AllayEntity> getBrain();
-
 	@Redirect(method = "canGather",
 	          at = @At(value = "INVOKE",
-	                   target = "Lnet/minecraft/world/GameRules;getBoolean(Lnet/minecraft/world/GameRules$Key;)Z"))
-	private boolean servux$fixAllayGathering1(GameRules instance, GameRules.Key<GameRules.BooleanRule> rule)
+	                   target = "Lnet/neoforged/neoforge/event/EventHooks;canEntityGrief(Lnet/minecraft/world/World;Lnet/minecraft/entity/Entity;)Z"))
+	private static boolean servux$fixAllayGathering(World world, Entity entity)
 	{
-		if (EntitiesDataProvider.INSTANCE.hasFixAllayGathering())
+		if (EntitiesDataProvider.INSTANCE.hasFixAllayGathering() &&
+			entity != null && entity.getType() == EntityType.ALLAY)
 		{
 			return true;
 		}
 
-		return instance.getBoolean(rule);
+		return net.neoforged.neoforge.event.EventHooks.canEntityGrief(world, entity);
 	}
-
-//	@Inject(method = "isItemPickupCoolingDown",
-//	        at = @At("RETURN"), cancellable = true)
-//	private void servux$fixAllayGathering2(CallbackInfoReturnable<Boolean> cir)
-//	{
-//		if (EntitiesDataProvider.INSTANCE.hasFixAllayGathering() &&
-//			cir.getReturnValue())
-//		{
-//			cir.setReturnValue(false);
-//		}
-//	}
 }
